86c428bbada865b32e3082ae2cfc84d682273631
AdriVelasco2 <isadri35@hotmail.com>
2024-05-21 19:54:43 +0200

    reports added

--- hellocucumber/src/test/java/hellocucumber/RunCucumberTest.java
+++ hellocucumber/src/test/java/hellocucumber/RunCucumberTest.java
@@ -1,15 +1,14 @@
 package hellocucumber;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("hellocucumber")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = { "pretty", "html:src/test/resources/reports/cucumber-html-report.html", "json:resources/cucumber.json"},
+        glue = {"hellocucumber"},
+        features = {"src/test/resources/hellocucumber/"}
+)
 public class RunCucumberTest {
 }
